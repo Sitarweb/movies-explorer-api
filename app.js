43b80cf674235cdf1f617ec440f111edf7f1b640
Simon Tarasenko <semeontarasenko@yandex.ru>
2023-08-08 18:46:03 +0300

    Добавляет cors

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes/index');
 const errorHandler = require('./middlewares/error-handler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
+app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
